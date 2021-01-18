a369809b851859aaada7deb7909f682ef6d6797b
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-17 19:56:15 -0700

    Number buttons send numbers to calculator screen

--- calc.js
+++ calc.js
@@ -21,7 +21,7 @@ const buttonObj = {};
 const buttons = Object.freeze({
   add: ['+', 'operator', 1, 1],
   subtract: ['-', 'operator', 1, 1],
-  multiply: ['x', 'operator', 1, 1],
+  multiply: ['*', 'operator', 1, 1],
   divide: ['/', 'operator', 1, 1],
   equals: ['=', 'special', 1, 1],
   clear: ['C', 'special', 1, 1],
@@ -30,4 +30,27 @@ const buttons = Object.freeze({
 });
 
 // DOM
+const calcScreen = document.querySelector('#num-input');
 const btnCntr = document.querySelector('.btn-group');
+const buttonElms = btnCntr.querySelectorAll('button');
+
+buttonElms.forEach(btn => {
+  const btnClasses = btn.className;
+  if (btnClasses.includes('num-btn')) {
+    btn.addEventListener('click', () => {
+      sendDigit(btn.getAttribute('data-num'));
+    })
+    console.log(`${btn} is num`);
+  } else if (btnClasses.includes('op-btn')) {
+    console.log(`${btn} is op`);
+  } else if (btnClasses.includes('spc-btn')) {
+    console.log(`${btn} is spc`);
+  } else {
+    console.warn("invalid btn found");
+  }
+});
+
+// Sent from a number button to the calculator "screen"
+function sendDigit(n) {
+  calcScreen.textContent += n;
+}
