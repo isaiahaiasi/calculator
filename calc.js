debf8fe435e53182888cd752b8c8f9ff517c5eda
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-17 23:57:23 -0700

    Make minor fixes to simple operation handling

--- calc.js
+++ calc.js
@@ -1,16 +1,10 @@
 const MAX_NUM_LEN = 10;
 
-// For getting number from argA/argB strings.
-// idk if it's necessary or not with how js handles types...
-function getNum(a) {
-  return a ? +a : 0;
-}
-
 // BASIC OPERATIONS
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => b != 0 ? a / b : 2318008;
 
 // Don't really have to freeze, but I wanted something more like an enum
 const ops = Object.freeze({
@@ -18,6 +12,13 @@ const ops = Object.freeze({
   subtract,
   multiply,
   divide,
+});
+
+const opSymbols = Object.freeze({
+  'add':'+',
+  'subtract':'-',
+  'multiply':'*',
+  'divide': '/',
 });
 
 function operate(op, a, b) {
@@ -81,21 +82,19 @@ function sendDigit(n) {
 }
 
 // Called when an op button is pressed
-// - validate (is there a full clause?)
-// - if not, return false
-// - if so:
-//    - evaluate operate(loggedOp, +argA, +argB)
-//    - set argA = result & update top screen
-//    - set passed op as loggedOp
-//    - wipe argB & calcScreen
+// TODO: Probably needs better validation...
 function performOp(op) {
-  let val = operate(loggedOp, +argA, +argB);
+  let val = operate(loggedOp, +argA, +argB); 
+
+  console.log(`${argA} ${opSymbols[loggedOp]} ${argB} = ${val}`);
+
+  // TODO: round val before putting it out to the screen
+
   loggedOp = op;
   argA = val;
-  calcTopScreen.textContent = argA;
+  calcTopScreen.textContent = argA + " " + opSymbols[op];
   calcScreen.textContent = "";
   argB = "";
-  console.log(val);
 }
 
 
